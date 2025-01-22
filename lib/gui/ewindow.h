2c31834ec07774f6b7b5fb02138709e06ed46cd6
RedouaneTM@ <redouane343@yahoo.com>
2025-01-22 14:44:55 +0100

    Update ewindow.h

--- lib/gui/ewindow.h
+++ lib/gui/ewindow.h
@@ -3,39 +3,48 @@
 
 #include <lib/gui/ewidget.h>
 #include <lib/gui/ewindowstyle.h>
+#include <lib/gui/eglsanimation.h>
 
 class eWidgetDesktop;
 
 class eWindow: public eWidget
 {
-	friend class eWindowStyle;
+    friend class eWindowStyle;
 public:
-	eWindow(eWidgetDesktop *desktop, int z = 0);
-	~eWindow();
-	void setTitle(const std::string &string);
-	std::string getTitle() const;
-	eWidget *child() { return m_child; }
+    eWindow(eWidgetDesktop *desktop, int z = 0);
+    ~eWindow();
+    void setTitle(const std::string &string);
+    std::string getTitle() const;
+    eWidget *child() { return m_child; }
 
-	enum {
-		wfNoBorder = 1
-	};
+    enum {
+        wfNoBorder = 1,
+        flagHasTrans = 2,
+        flagNoAnimated = 4
+    };
 
-	void setBackgroundColor(const gRGB &col);
-	void setCornerRadius(int radius, int edges);
+    void setBackgroundColor(const gRGB &col);
 
-	void setFlag(int flags);
-	void clearFlag(int flags);
+    void setFlag(int flags);
+    void clearFlag(int flags);
+    
+    // GLS Animation support
+    void setAnimation(eGLSAnimation::AnimationType type, int duration = 500);
+    void clearAnimation();
+
 protected:
-	enum eWindowEvents
-	{
-		evtTitleChanged = evtUserWidget,
-	};
-	int event(int event, void *data=0, void *data2=0);
+    enum eWindowEvents
+    {
+        evtTitleChanged = evtUserWidget,
+    };
+    int event(int event, void *data=0, void *data2=0);
+
 private:
-	std::string m_title;
-	eWidget *m_child;
-	int m_flags;
-	eWidgetDesktop *m_desktop;
+    std::string m_title;
+    eWidget *m_child;
+    int m_flags;
+    eWidgetDesktop *m_desktop;
+    ePtr<eGLSAnimation> m_animation;
 };
 
 #endif
