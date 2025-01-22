5d1acaf3a7c6f42671b563ce1e202cf3befaa0f7
RedouaneTM@ <redouane343@yahoo.com>
2025-01-22 15:44:15 +0100

    Update eglsanimation.h

--- lib/gui/eglsanimation.h
+++ lib/gui/eglsanimation.h
@@ -3,7 +3,7 @@
 
 #include <lib/base/object.h>
 #include <lib/gdi/gpixmap.h>
-#include <lib/base/etimer.h>
+#include <lib/base/ebase.h>
 #include <lib/gui/ewidget.h>
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
