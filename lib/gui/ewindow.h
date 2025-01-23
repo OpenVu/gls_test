9c2ce73234415ed243b55f51c5d1fd49cad66c23
RedouaneTM@ <redouane343@yahoo.com>
2025-01-23 11:57:18 +0100

    Update ewindow.h

--- lib/gui/ewindow.h
+++ lib/gui/ewindow.h
@@ -9,27 +9,38 @@ class eWindow: public eWidget
     DECLARE_REF(eWindow);
 public:
     enum {
-        wfNoBorder = 1
+        wfNoBorder = 1,
+        flagHasTrans = 2,
+        flagNoAnimated = 4
     };
+    
     eWindow(eWidget *parent, int flags=0);
     ~eWindow();
+    
     void setTitle(const std::string &string);
-    void setFlags(int flags);
-    void clearFlags(int flags);
+    std::string getTitle() const { return m_title; }
+    void setBackgroundColor(const gRGB &col);
+    
+    void setFlags(int flags) { m_flags |= flags; }
+    void clearFlags(int flags) { m_flags &= ~flags; }
+    
     void setAnimation(eGLSAnimationType type, int duration = 500);
     void clearAnimation();
     
 protected:
     int event(int event, void *data=0, void *data2=0);
+    
 private:
     enum eWindowEvents
     {
         evtTitleChanged = evtUserWidget,
     };
+    
     ePtr<eGLSAnimation> m_animation;
     eGLSAnimationParams *m_animation_params;
     std::string m_title;
     int m_flags;
+    eWidget *m_child;
 };
 
 #endif
