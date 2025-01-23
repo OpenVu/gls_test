a924b7c61172e407d3b3a1143f3938dcc2b50a30
RedouaneTM@ <redouane343@yahoo.com>
2025-01-23 10:53:15 +0100

    Update eglsanimation.h

--- lib/gui/eglsanimation.h
+++ lib/gui/eglsanimation.h
@@ -2,61 +2,50 @@
 #define __lib_gui_eglsanimation_h
 
 #include <lib/base/object.h>
-#include <lib/base/ebase.h>
-#include <lib/gui/ewidget.h>
 #include <lib/gdi/gpixmap.h>
+#include <lib/gui/ewidget.h>
+#include <lib/gui/ewindow.h>
 
-#ifdef HAVE_MALI
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
-#else
-// Dummy types when no EGL/GLES2 is available
-typedef void* EGLDisplay;
-typedef void* EGLSurface;
-typedef void* EGLContext;
-typedef void* EGLConfig;
-typedef unsigned int GLuint;
-#endif
-
-enum eGLSAnimationType {
-    TYPE_FADE = 0,
-    TYPE_SLIDE = 1,
-    TYPE_ZOOM = 2,
-    TYPE_ROTATE = 3,
-    TYPE_BOUNCE = 4,
-    TYPE_SHAKE = 5,
-    TYPE_BLUR = 6,
-    TYPE_FLIP = 7
+enum eGLSAnimationType
+{
+    TYPE_FADE,
+    TYPE_SLIDE,
+    TYPE_ZOOM,
+    TYPE_ROTATE,
+    TYPE_BOUNCE,
+    TYPE_SHAKE
 };
 
-enum eGLSEasingType {
-    EASING_LINEAR = 0,
-    EASING_SINE_IN = 1,
-    EASING_SINE_OUT = 2,
-    EASING_SINE_IN_OUT = 3,
-    EASING_QUAD_IN = 4,
-    EASING_QUAD_OUT = 5,
-    EASING_QUAD_IN_OUT = 6,
-    EASING_CUBIC_IN = 7,
-    EASING_CUBIC_OUT = 8,
-    EASING_CUBIC_IN_OUT = 9,
-    EASING_BOUNCE_IN = 10,
-    EASING_BOUNCE_OUT = 11,
-    EASING_BOUNCE_IN_OUT = 12,
-    EASING_ELASTIC_IN = 13,
-    EASING_ELASTIC_OUT = 14,
-    EASING_ELASTIC_IN_OUT = 15,
-    EASING_BACK_IN = 16,
-    EASING_BACK_OUT = 17,
-    EASING_BACK_IN_OUT = 18
+enum eGLSEasingType
+{
+    EASING_LINEAR,
+    EASING_SINE_IN,
+    EASING_SINE_OUT,
+    EASING_SINE_IN_OUT,
+    EASING_QUAD_IN,
+    EASING_QUAD_OUT,
+    EASING_QUAD_IN_OUT,
+    EASING_CUBIC_IN,
+    EASING_CUBIC_OUT,
+    EASING_CUBIC_IN_OUT,
+    EASING_BOUNCE_IN,
+    EASING_BOUNCE_OUT,
+    EASING_BOUNCE_IN_OUT,
+    EASING_ELASTIC_IN,
+    EASING_ELASTIC_OUT,
+    EASING_ELASTIC_IN_OUT,
+    EASING_BACK_IN,
+    EASING_BACK_OUT,
+    EASING_BACK_IN_OUT
 };
 
-struct eGLSAnimationParams {
+struct eGLSAnimationParams
+{
     eGLSAnimationType type;
     eGLSEasingType easing;
-    int startValue;
-    int endValue;
-    int duration;  // in milliseconds
+    int duration;
+    float startValue;
+    float endValue;
     ePoint startPos;
     ePoint endPos;
     ePoint center;
@@ -67,46 +56,33 @@ struct eGLSAnimationParams {
     eGLSAnimationParams() : 
         type(TYPE_FADE),
         easing(EASING_SINE_IN_OUT),
+        duration(1000),
         startValue(0),
         endValue(100),
-        duration(1000),
         rotationAngle(0),
         bounceCount(3),
         amplitude(1.0f)
     {}
 };
 
-class eGLSAnimation : public iObject
+class eGLSAnimation: public Object
 {
     DECLARE_REF(eGLSAnimation);
-
-private:
-    eWidget *m_widget;
-    ePtr<eTimer> m_timer;
-    eGLSAnimationParams m_params;
-    int m_current_tick;
-    int m_total_ticks;
-    bool m_active;
-    ePtr<eGLSAnimation> m_nextAnimation;  // For animation chaining
-
-#ifdef HAVE_MALI
-    EGLDisplay m_eglDisplay;
-    EGLContext m_eglContext;
-    EGLSurface m_eglSurface;
-    GLuint m_program;
-    GLuint m_texture;
-#endif
-
-    void applyFade(float progress);
-    void applySlide(float progress);
-    void applyZoom(float progress);
-    void applyRotate(float progress);
-    void applyBounce(float progress);
-    void applyShake(float progress);
-    void applyBlur(float progress);
-    void applyFlip(float progress);
     
-    float applyEasing(float progress);
+public:
+    eGLSAnimation(eWidget *widget);
+    virtual ~eGLSAnimation();
+    
+    void start(const eGLSAnimationParams &params);
+    void stop();
+    void chain(eGLSAnimation *next);
+    eWidget *getWidget() const { return m_widget; }
+    
+protected:
+    void timerTick();
+    float applyEasing(float t);
+    
+    // Easing functions
     float bounceEaseIn(float t);
     float bounceEaseOut(float t);
     float bounceEaseInOut(float t);
@@ -116,36 +92,26 @@ private:
     float backEaseIn(float t);
     float backEaseOut(float t);
     float backEaseInOut(float t);
-
-#ifdef HAVE_MALI
-    bool initEGL();
-    void cleanupEGL();
-    bool createShaders();
-#endif
-
-protected:
-    void timerTick();
+    
+    // Animation application functions
+    void applyFade(float progress);
+    void applySlide(float progress);
+    void applyZoom(float progress);
+    void applyRotate(float progress);
+    void applyBounce(float progress);
+    void applyShake(float progress);
+    
+private:
+    eWidget *m_widget;
+    eTimer *m_timer;
+    eGLSAnimationParams m_params;
+    int m_current_tick;
+    int m_total_ticks;
+    bool m_active;
+    ePtr<eGLSAnimation> m_next_animation;
+    
     void onAnimationFinished();
-
-public:
-    eGLSAnimation(eWidget *widget);
-    virtual ~eGLSAnimation();
-
-    void start(const eGLSAnimationParams &params);
-    void stop();
-    void pause();
-    void resume();
-
-    // Animation chaining
-    void chain(eGLSAnimation *nextAnimation);
-    void clearChain();
-
-    // Getters
-    eWidget *getWidget() const { return m_widget; }
-    bool isActive() const { return m_active; }
-    const eGLSAnimationParams &getParams() const { return m_params; }
-
-    PSignal0<void> animationFinished;
+    bool initEGL();
 };
 
 #endif // __lib_gui_eglsanimation_h
