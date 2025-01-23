317f8c6b89e23dabc642cc6c29d0b022eec5431d
RedouaneTM@ <redouane343@yahoo.com>
2025-01-23 12:03:50 +0100

    Update eglsanimation.h

--- lib/gui/eglsanimation.h
+++ lib/gui/eglsanimation.h
@@ -3,118 +3,89 @@
 
 #include <lib/base/object.h>
 #include <lib/base/ebase.h>
-#include <lib/gdi/gpixmap.h>
 #include <lib/gui/ewidget.h>
+#include <lib/gdi/gpixmap.h>
 
-enum eGLSAnimationType
-{
-    TYPE_FADE,
-    TYPE_SLIDE,
-    TYPE_ZOOM,
-    TYPE_ROTATE,
-    TYPE_BOUNCE,
-    TYPE_SHAKE
+#ifdef HAVE_MALI
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#else
+// Dummy types when no EGL/GLES2 is available
+typedef void* EGLDisplay;
+typedef void* EGLSurface;
+typedef void* EGLContext;
+typedef void* EGLConfig;
+typedef unsigned int GLuint;
+#endif
+
+enum eGLSAnimationType {
+    TYPE_FADE = 0,
+    TYPE_SLIDE = 1,
+    TYPE_ZOOM = 2
 };
 
-enum eGLSEasingType
-{
-    EASING_LINEAR,
-    EASING_SINE_IN,
-    EASING_SINE_OUT,
-    EASING_SINE_IN_OUT,
-    EASING_QUAD_IN,
-    EASING_QUAD_OUT,
-    EASING_QUAD_IN_OUT,
-    EASING_CUBIC_IN,
-    EASING_CUBIC_OUT,
-    EASING_CUBIC_IN_OUT,
-    EASING_BOUNCE_IN,
-    EASING_BOUNCE_OUT,
-    EASING_BOUNCE_IN_OUT,
-    EASING_ELASTIC_IN,
-    EASING_ELASTIC_OUT,
-    EASING_ELASTIC_IN_OUT,
-    EASING_BACK_IN,
-    EASING_BACK_OUT,
-    EASING_BACK_IN_OUT
-};
-
-struct eGLSAnimationParams
-{
+struct eGLSAnimationParams {
     eGLSAnimationType type;
-    eGLSEasingType easing;
-    int duration;
-    int startValue;  // Changed to int for format string compatibility
-    int endValue;    // Changed to int for format string compatibility
+    int startValue;
+    int endValue;
+    int duration;  // in milliseconds
     ePoint startPos;
     ePoint endPos;
     ePoint center;
-    float rotationAngle;
-    int bounceCount;
-    float amplitude;
-    
+
     eGLSAnimationParams() : 
         type(TYPE_FADE),
-        easing(EASING_SINE_IN_OUT),
-        duration(1000),
         startValue(0),
         endValue(100),
-        rotationAngle(0),
-        bounceCount(3),
-        amplitude(1.0f)
-    {
-    }
+        duration(1000) {}
 };
 
-class eGLSAnimation: public iObject
+class eGLSAnimation : public iObject
 {
     DECLARE_REF(eGLSAnimation);
-    
-public:
-    eGLSAnimation(eWidget *widget);
-    virtual ~eGLSAnimation();
-    
-    void start(const eGLSAnimationParams &params);
-    void stop();
-    void chain(eGLSAnimation *next);
-    eWidget *getWidget() const { return m_widget; }
-    
-    PSignal0<void> animationFinished;  // Changed to PSignal0
-    
-protected:
-    void timerTick();
-    float applyEasing(float t);
-    
-    // Easing functions
-    float bounceEaseIn(float t);
-    float bounceEaseOut(float t);
-    float bounceEaseInOut(float t);
-    float elasticEaseIn(float t);
-    float elasticEaseOut(float t);
-    float elasticEaseInOut(float t);
-    float backEaseIn(float t);
-    float backEaseOut(float t);
-    float backEaseInOut(float t);
-    
-    // Animation application functions
-    void applyFade(float progress);
-    void applySlide(float progress);
-    void applyZoom(float progress);
-    void applyRotate(float progress);
-    void applyBounce(float progress);
-    void applyShake(float progress);
-    
+
 private:
     eWidget *m_widget;
-    ePtr<eTimer> m_timer;  // Changed to ePtr
+    ePtr<eTimer> m_timer;
     eGLSAnimationParams m_params;
     int m_current_tick;
     int m_total_ticks;
     bool m_active;
-    ePtr<eGLSAnimation> m_next_animation;
-    
-    void onAnimationFinished();
+
+#ifdef HAVE_MALI
+    EGLDisplay m_eglDisplay;
+    EGLContext m_eglContext;
+    EGLSurface m_eglSurface;
+    GLuint m_program;
+    GLuint m_texture;
+#endif
+
+    void applyFade(float progress);
+    void applySlide(float progress);
+    void applyZoom(float progress);
+
+#ifdef HAVE_MALI
     bool initEGL();
+    void cleanupEGL();
+    bool createShaders();
+#endif
+
+protected:
+    void timerTick();
+
+public:
+    eGLSAnimation(eWidget *widget);
+    virtual ~eGLSAnimation();
+
+    void start(const eGLSAnimationParams &params);
+    void stop();
+    void pause();
+    void resume();
+    void tick();
+
+    PSignal0<void> animationFinished;
+
+    bool isRunning() const { return m_active; }
 };
 
 #endif // __lib_gui_eglsanimation_h
