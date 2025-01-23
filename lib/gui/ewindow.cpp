b7a06b0e6726b2cf35679a1642047607c3df8df6
RedouaneTM@ <redouane343@yahoo.com>
2025-01-23 12:01:15 +0100

    Update ewindow.cpp

--- lib/gui/ewindow.cpp
+++ lib/gui/ewindow.cpp
@@ -1,51 +1,108 @@
-#include "ewindow.h"
+#include <lib/gui/ewindow.h>
 #include <lib/gui/ewidgetdesktop.h>
 
-DEFINE_REF(eWindow);
+#include <lib/gui/ewindowstyle.h>
+#include <lib/gui/ewindowstyleskinned.h>
 
-eWindow::eWindow(eWidget *parent, int flags)
-    : eWidget(parent)
-    , m_flags(flags)
-    , m_child(0)
-    , m_animation_params(0)
+#include <lib/gdi/epng.h>
+#include <lib/gui/eglsanimation.h>
+
+eWindow::eWindow(eWidgetDesktop *desktop, int z): eWidget(0)
 {
-    m_animation = 0;
+	m_flags = 0;
+	m_desktop = desktop;
+	m_animation = nullptr;
+		/* ask style manager for current style */
+	ePtr<eWindowStyleManager> mgr;
+	eWindowStyleManager::getInstance(mgr);
+
+	ePtr<eWindowStyle> style;
+	if (mgr)
+		mgr->getStyle(desktop->getStyleID(), style);
+
+		/* when there is either no style manager or no style, revert to simple style. */
+	if (!style)
+		style = new eWindowStyleSimple();
+
+	setStyle(style);
+
+	setZPosition(z); /* must be done before addRootWidget */
+
+		/* we are the parent for the child window. */
+		/* as we are in the constructor, this is thread safe. */
+	m_child = this;
+	m_child = new eWidget(this);
+	desktop->addRootWidget(this);
 }
 
 eWindow::~eWindow()
 {
-    if (m_child)
-        delete m_child;
-    if (m_animation_params)
-        delete m_animation_params;
+	m_desktop->removeRootWidget(this);
+	m_child->destruct();
 }
 
 void eWindow::setTitle(const std::string &string)
 {
-    m_title = string;
-    event(evtTitleChanged);
+	if (m_title == string)
+		return;
+	m_title = string;
+	event(evtTitleChanged);
+}
+
+std::string eWindow::getTitle() const
+{
+	return m_title;
 }
 
 void eWindow::setBackgroundColor(const gRGB &col)
 {
-    eWidget::setBackgroundColor(col);
+		/* set background color for child, too */
+	eWidget::setBackgroundColor(col);
+	m_child->setBackgroundColor(col);
+}
+
+void eWindow::setFlag(int flags)
+{
+	m_flags |= flags;
+}
+
+void eWindow::clearFlag(int flags)
+{
+	m_flags &= ~flags;
 }
 
 void eWindow::setAnimation(eGLSAnimationType type, int duration)
 {
-    if (m_flags & flagNoAnimated)
-        return;
-        
-    if (!m_animation_params)
-        m_animation_params = new eGLSAnimationParams();
-        
-    m_animation_params->type = type;
-    m_animation_params->duration = duration;
-    
     if (!m_animation)
         m_animation = new eGLSAnimation(this);
-        
-    m_animation->start(*m_animation_params);
+
+    //eGLSAnimation::AnimationParams params;
+    eGLSAnimationParams params;	
+    params.type = type;
+    params.duration = duration;
+    
+    switch (type)
+    {
+        case TYPE_FADE:
+            params.startValue = 0;
+            params.endValue = 100;
+            break;
+            
+        case TYPE_SLIDE:
+            {
+                ePoint current = position();
+                params.startPos = ePoint(current.x() - 100, current.y());
+                params.endPos = current;
+            }
+            break;
+            
+        case TYPE_ZOOM:
+            params.startValue = 50;
+            params.endValue = 100;
+            break;
+    }
+    
+    m_animation->start(params);
 }
 
 void eWindow::clearAnimation()
@@ -53,44 +110,51 @@ void eWindow::clearAnimation()
     if (m_animation)
     {
         m_animation->stop();
-        m_animation = 0;
+        m_animation = nullptr;
     }
 }
 
 int eWindow::event(int event, void *data, void *data2)
 {
-    switch (event)
-    {
-        case evtPaint:
-        {
-            ePtr<eWindowStyle> style;
-            if (!getStyle(style))
-            {
-                gPainter &painter = *(gPainter*)data2;
-                
-                // Draw window background
-                painter.clear();
-                
-                // Draw window frame if needed
-                if (!(m_flags & wfNoBorder))
-                {
-                    style->drawFrame(painter, eRect(ePoint(0, 0), size()), eWindowStyle::frameGeneric);
-                }
-            }
-            return 0;
-        }
-        case evtReceivedFocus:
-        case evtLostFocus:
-            break;
-        case evtSizeChanged:
-        {
-            eSize size = *static_cast<eSize*>(data);
-            if (m_child)
-                m_child->resize(size);
-            break;
-        }
-        default:
-            break;
-    }
-    return eWidget::event(event, data, data2);
+	switch (event)
+	{
+	case evtWillChangeSize:
+	{
+		eSize &new_size = *static_cast<eSize*>(data);
+		eSize &offset = *static_cast<eSize*>(data2);
+		if (!(m_flags & wfNoBorder))
+		{
+			ePtr<eWindowStyle> style;
+			if (!getStyle(style))
+			{
+//			eDebug("[eWindow] evtWillChangeSize to %d %d", new_size.width(), new_size.height());
+				style->handleNewSize(this, new_size, offset);
+			}
+		} else
+			m_child->resize(new_size);
+		break;
+	}
+	case evtPaint:
+	{
+		if (!(m_flags & wfNoBorder))
+		{
+			ePtr<eWindowStyle> style;
+			if (!getStyle(style))
+			{
+				gPainter &painter = *static_cast<gPainter*>(data2);
+				style->paintWindowDecoration(this, painter, m_title);
+			}
+		}
+		return 0;
+	}
+	case evtTitleChanged:
+			/* m_visible_region contains, in contrast to m_visible_with_childs,
+			   only the decoration. though repainting the whole decoration is bad,
+			   repainting the whole window is even worse. */
+		invalidate(m_visible_region);
+		break;
+	default:
+		break;
+	}
+	return eWidget::event(event, data, data2);
 }
