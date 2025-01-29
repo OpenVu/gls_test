5cdfa88dbab23eaf1e64f02573c6cd5afd45c1e8
RedouaneTM@ <redouane343@yahoo.com>
2025-01-29 10:26:02 +0100

    Update ewindow.cpp

--- lib/gui/ewindow.cpp
+++ lib/gui/ewindow.cpp
@@ -1,107 +1,127 @@
 #include <lib/gui/ewindow.h>
 #include <lib/gui/ewidgetdesktop.h>
-
 #include <lib/gui/ewindowstyle.h>
 #include <lib/gui/ewindowstyleskinned.h>
-
 #include <lib/gdi/epng.h>
 #include <lib/gui/eglsanimation.h>
 
 eWindow::eWindow(eWidgetDesktop *desktop, int z): eWidget(0)
 {
-	m_flags = 0;
-	m_desktop = desktop;
-	m_animation = nullptr;
-		/* ask style manager for current style */
-	ePtr<eWindowStyleManager> mgr;
-	eWindowStyleManager::getInstance(mgr);
-
-	ePtr<eWindowStyle> style;
-	if (mgr)
-		mgr->getStyle(desktop->getStyleID(), style);
-
-		/* when there is either no style manager or no style, revert to simple style. */
-	if (!style)
-		style = new eWindowStyleSimple();
-
-	setStyle(style);
-
-	setZPosition(z); /* must be done before addRootWidget */
-
-		/* we are the parent for the child window. */
-		/* as we are in the constructor, this is thread safe. */
-	m_child = this;
-	m_child = new eWidget(this);
-	desktop->addRootWidget(this);
+    m_flags = 0;
+    m_desktop = desktop;
+    m_animation = nullptr;
+    
+    ePtr<eWindowStyleManager> mgr;
+    eWindowStyleManager::getInstance(mgr);
+    
+    ePtr<eWindowStyle> style;
+    if (mgr)
+        mgr->getStyle(desktop->getStyleID(), style);
+    
+    if (!style)
+        style = new eWindowStyleSimple();
+    
+    setStyle(style);
+    setZPosition(z);
+    
+    m_child = this;
+    m_child = new eWidget(this);
+    desktop->addRootWidget(this);
 }
 
 eWindow::~eWindow()
 {
-	m_desktop->removeRootWidget(this);
-	m_child->destruct();
+    if (m_animation)
+    {
+        m_animation->stop();
+        delete m_animation;
+        m_animation = nullptr;
+    }
+    m_desktop->removeRootWidget(this);
+    m_child->destruct();
 }
 
 void eWindow::setTitle(const std::string &string)
 {
-	if (m_title == string)
-		return;
-	m_title = string;
-	event(evtTitleChanged);
+    if (m_title == string)
+        return;
+    m_title = string;
+    event(evtTitleChanged);
 }
 
 std::string eWindow::getTitle() const
 {
-	return m_title;
+    return m_title;
 }
 
 void eWindow::setBackgroundColor(const gRGB &col)
 {
-		/* set background color for child, too */
-	eWidget::setBackgroundColor(col);
-	m_child->setBackgroundColor(col);
+    eWidget::setBackgroundColor(col);
+    m_child->setBackgroundColor(col);
 }
 
 void eWindow::setFlag(int flags)
 {
-	m_flags |= flags;
+    m_flags |= flags;
 }
 
 void eWindow::clearFlag(int flags)
 {
-	m_flags &= ~flags;
+    m_flags &= ~flags;
 }
 
-void eWindow::setAnimation(eGLSAnimationType type, int duration)
+void eWindow::setAnimation(eGLSAnimationParams::AnimationType type, int duration, int fps)
 {
     if (!m_animation)
         m_animation = new eGLSAnimation(this);
-
-    //eGLSAnimation::AnimationParams params;
-    eGLSAnimationParams params;	
+    
+    eGLSAnimationParams params;
     params.type = type;
     params.duration = duration;
+    params.fps = fps ? fps : 60;  // Default to 60fps if not specified
     
+    // Configure animation parameters based on type
     switch (type)
     {
-        case TYPE_FADE:
-            params.startValue = 0;
-            params.endValue = 100;
+        case eGLSAnimationParams::TYPE_FADE:
+            params.startValue = 0;    // Start fully transparent
+            params.endValue = 100;    // End fully opaque
             break;
             
-        case TYPE_SLIDE:
+        case eGLSAnimationParams::TYPE_SLIDE:
             {
                 ePoint current = position();
-                params.startPos = ePoint(current.x() - 100, current.y());
+                eSize screen = m_desktop->size();
+                
+                // Start from left edge of screen
+                params.startPos = ePoint(-size().width(), current.y());
                 params.endPos = current;
+                
+                // Set easing for smooth slide
+                params.easing = 3;  // Cubic easing
             }
             break;
             
-        case TYPE_ZOOM:
-            params.startValue = 50;
-            params.endValue = 100;
+        case eGLSAnimationParams::TYPE_ZOOM:
+            {
+                params.startValue = 50;     // Start at 50% size
+                params.endValue = 100;      // End at 100% size
+                
+                // Calculate center point for zoom
+                ePoint current = position();
+                eSize windowSize = size();
+                params.center = ePoint(
+                    current.x() + (windowSize.width() / 2),
+                    current.y() + (windowSize.height() / 2)
+                );
+                
+                // Set easing for smooth zoom
+                params.easing = 3;  // Cubic easing
+            }
             break;
     }
     
+    // Start the buffered animation
     m_animation->start(params);
 }
 
@@ -110,51 +130,28 @@ void eWindow::clearAnimation()
     if (m_animation)
     {
         m_animation->stop();
+        delete m_animation;
         m_animation = nullptr;
     }
 }
 
 int eWindow::event(int event, void *data, void *data2)
 {
-	switch (event)
-	{
-	case evtWillChangeSize:
-	{
-		eSize &new_size = *static_cast<eSize*>(data);
-		eSize &offset = *static_cast<eSize*>(data2);
-		if (!(m_flags & wfNoBorder))
-		{
-			ePtr<eWindowStyle> style;
-			if (!getStyle(style))
-			{
-//			eDebug("[eWindow] evtWillChangeSize to %d %d", new_size.width(), new_size.height());
-				style->handleNewSize(this, new_size, offset);
-			}
-		} else
-			m_child->resize(new_size);
-		break;
-	}
-	case evtPaint:
-	{
-		if (!(m_flags & wfNoBorder))
-		{
-			ePtr<eWindowStyle> style;
-			if (!getStyle(style))
-			{
-				gPainter &painter = *static_cast<gPainter*>(data2);
-				style->paintWindowDecoration(this, painter, m_title);
-			}
-		}
-		return 0;
-	}
-	case evtTitleChanged:
-			/* m_visible_region contains, in contrast to m_visible_with_childs,
-			   only the decoration. though repainting the whole decoration is bad,
-			   repainting the whole window is even worse. */
-		invalidate(m_visible_region);
-		break;
-	default:
-		break;
-	}
-	return eWidget::event(event, data, data2);
+    switch (event)
+    {
+        case evtPaint:
+        {
+            ePtr<eWindowStyle> style;
+            getStyle(style);
+            
+            if (!style)
+                return -1;
+            
+            eWidget::event(event, data, data2);
+            
+            return 0;
+        }
+        default:
+            return eWidget::event(event, data, data2);
+    }
 }
